b37cb6239d109bee40f222a8892344110280b296
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2020-01-31 17:19:26 +0100

    Ajout de Sc2sa avec caseAPlusExp3

---
+++ src/sa/Sc2sa.java
@@ -0,0 +1,17 @@
+package sa;
+
+import sc.analysis.DepthFirstAdapter;
+import sc.node.APlusExp3;
+
+public class Sc2sa extends DepthFirstAdapter {
+    private SaNode returnValue;
+
+    public void caseAPlusExp3(APlusExp3 node){
+        SaExp op1, op2;
+        node.getExp3().apply(this);
+        op1 = (SaExp) this.returnValue;
+        node.getExp4().apply(this);
+        op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpAdd(op1, op2);
+    }
+}
