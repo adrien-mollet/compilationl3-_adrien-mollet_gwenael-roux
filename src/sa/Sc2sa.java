41dc34459b3bc90c7bf779370b2adf2d2b28c483
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2020-01-31 18:27:51 +0100

    Début implémentation Sc2sa

--- src/sa/Sc2sa.java
+++ src/sa/Sc2sa.java
@@ -1,10 +1,54 @@
 package sa;
 
 import sc.analysis.DepthFirstAdapter;
-import sc.node.APlusExp3;
+import sc.node.*;
 
 public class Sc2sa extends DepthFirstAdapter {
     private SaNode returnValue;
+
+    public void caseAAffectationI(AAffectationI node){
+        SaInstAffect op;
+        node.getIaff().apply(this);
+        op = (SaInstAffect) this.returnValue;
+        this.returnValue = op;
+    }
+
+    public void caseAAppelExp6(AAppelExp6 node){
+        SaExpAppel appel;
+        node.getApp().apply(this);
+        appel = (SaExpAppel) this.returnValue;
+        this.returnValue = new SaExpAppel(appel.getVal());
+    }
+
+    public void caseAAppelI(AAppelI node){
+        SaAppel appel;
+        node.getIapp().apply(this);
+        appel = (SaAppel) this.returnValue;
+        this.returnValue = appel;
+    }
+
+    public void caseABlocI(ABlocI node){
+        SaInstBloc bloc;
+        node.getIbloc().apply(this);
+        bloc = (SaInstBloc) this.returnValue;
+        this.returnValue = new SaInstBloc(bloc.getVal());
+    }
+
+    public void caseAConditionI(AConditionI node){
+        SaInstSi si;
+        node.getIsi().apply(this);
+        si = (SaInstSi) this.returnValue;
+        this.returnValue = si;
+    }
+
+    public void caseADf(ADf node){
+        /*TId id = node.getId();
+        SaLDec ldv;
+        SaLDec ldvo;
+        SaInstBloc ibloc;
+        node.getId().apply(this);*/
+
+    }
 
     public void caseAPlusExp3(APlusExp3 node){
         SaExp op1, op2;
