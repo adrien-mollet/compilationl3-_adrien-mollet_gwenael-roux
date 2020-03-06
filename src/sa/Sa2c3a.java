968f7527d9059a197b4e2f883b0e393c38eac298
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2020-03-06 17:19:00 +0100

    Petite avancée

--- src/sa/Sa2c3a.java
+++ src/sa/Sa2c3a.java
@@ -1,8 +1,6 @@
 package sa;
 
-import c3a.C3a;
-import c3a.C3aOperand;
-import c3a.C3aVar;
+import c3a.*;
 
 public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
 
@@ -15,32 +13,34 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
 
     @Override
     public C3aOperand visit(SaProg node) {
-        return super.visit(node);
+        return super.visit(node); //TODO
     }
 
     @Override
     public C3aOperand visit(SaDecTab node) {
-        return super.visit(node);
+        return super.visit(node); //TODO (A laisser ?)
     }
 
     @Override
     public C3aOperand visit(SaExp node) {
-        return super.visit(node);
+        return node.accept(this);
     }
 
     @Override
     public C3aOperand visit(SaExpInt node) {
-        return super.visit(node);
+        return new C3aConstant(node.getVal());
     }
 
     @Override
     public C3aOperand visit(SaExpVar node) {
-        return super.visit(node);
+        return node.accept(this);
     }
 
     @Override
     public C3aOperand visit(SaInstEcriture node) {
-        return super.visit(node);
+        C3aInstWrite inst = new C3aInstWrite(node.getArg().accept(this),"");
+        c3a.ajouteInst(inst);
+        return null;
     }
 
     @Override
