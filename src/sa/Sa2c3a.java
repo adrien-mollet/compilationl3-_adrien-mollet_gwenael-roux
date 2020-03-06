c9426e4050fdb694cd4ffdefe83b203e16187df9
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2020-03-06 16:44:11 +0100

    SaVarSimple et SaVarIndicee

--- src/sa/Sa2c3a.java
+++ src/sa/Sa2c3a.java
@@ -2,6 +2,7 @@ package sa;
 
 import c3a.C3a;
 import c3a.C3aOperand;
+import c3a.C3aVar;
 
 public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
 
@@ -12,6 +13,153 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         root.accept(this);
     }
 
+    @Override
+    public C3aOperand visit(SaProg node) {
+        return super.visit(node);
+    }
 
+    @Override
+    public C3aOperand visit(SaDecTab node) {
+        return super.visit(node);
+    }
 
+    @Override
+    public C3aOperand visit(SaExp node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpInt node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpVar node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstEcriture node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstTantQue node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaLInst node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaDecFonc node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaDecVar node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstAffect node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaLDec node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaVarSimple node) {
+        return new C3aVar(node.tsItem,null);
+    }
+
+    @Override
+    public C3aOperand visit(SaAppel node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAppel node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAdd node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpSub node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpMult node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpDiv node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpInf node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpEqual node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAnd node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpOr node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpNot node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaExpLire node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstBloc node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstSi node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaInstRetour node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaLExp node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public C3aOperand visit(SaVarIndicee node) {
+        return new C3aVar(node.tsItem,node.getIndice().accept(this));
+    }
 }
