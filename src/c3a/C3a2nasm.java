2767656ca16d572c39c8269ac036f9e944bdb123
r16014083 <gwenael.roux@etu.univ-amu.fr>
2020-03-13 17:10:40 +0100

    before pull

--- src/c3a/C3a2nasm.java
+++ src/c3a/C3a2nasm.java
@@ -1,6 +1,8 @@
 package c3a;
 
 import nasm.Nasm;
+import nasm.NasmAdd;
+import nasm.NasmMov;
 import nasm.NasmOperand;
 import ts.Ts;
 import ts.TsItemFct;
@@ -25,6 +27,15 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
 
     @Override
     public NasmOperand visit(C3aInstAdd inst) {
+        NasmOperand label = null;
+        if (inst.label != null){
+            label = inst.label.accept(this);
+        }
+        NasmOperand op1 = inst.op1.accept(this);
+        NasmOperand op2 = inst.op2.accept(this);
+        NasmOperand dest = inst.result.accept(this);
+        nasm.ajouteInst(new NasmMov(label,dest,op1,""));
+        nasm.ajouteInst(new NasmAdd(null,dest,op2,""));
         return null;
     }
 
